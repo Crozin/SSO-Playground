94c586bbeb404a0ad21ae99b14f3de6a5bcf0338
Krzysztof Łabuś <krzysztof.labus@pracuj.pl>
2018-01-22 12:03:08 +0100

    Added 2FA

---
+++ Website1/Controllers/BuyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace Website1.Controllers
+{
+    public class BuyController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            var cs = (ClaimsPrincipal) User;
+
+            if (cs.Claims.Any(c => c.Type == "amr" && c.Value == "2fa"))
+            {
+
+                return View("Index");
+            }
+            else
+            {
+                return View("Upgrade2Fa");
+            }
+        }
+    }
+}
